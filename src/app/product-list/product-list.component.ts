6b3767940c0acd2cd9e3309b26aef8aa9836bb5c
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-21 16:36:20 +0700

    edit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,19 +3,17 @@ import { Product } from './../model/product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
-    const p1 = new Product('01', 'Name 01', 100.0);
-    const p2 = new Product('02', 'Name 02', 200.5);
-    const p3 = new Product('03', 'Name 03', 300.75);
+    const p1 = new Product('0001', 'AA', 130.0, 'Y', 5);
+    const p2 = new Product('0002', 'BBB', 20.5, 'N', 10);
+    const p3 = new Product('0003', 'CCCC', 30.75, 'Y', 100);
     this.products.push(p1, p2, p3);
   }
-
 }
