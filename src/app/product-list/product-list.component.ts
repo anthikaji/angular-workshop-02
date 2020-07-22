9e37922f124ba894c4f7188b643d76aa63aaacc6
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-22 10:33:22 +0700

    edit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './../model/product';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -11,7 +12,7 @@ export class ProductListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const p1 = new Product('0001', 'AA', 130.0, 'Y', 5);
+    const p1 = new Product('0001', 'AA', 130, 'Y', 5);
     const p2 = new Product('0002', 'BBB', 20.5, 'N', 10);
     const p3 = new Product('0003', 'CCCC', 30.75, 'Y', 100);
     this.products.push(p1, p2, p3);
